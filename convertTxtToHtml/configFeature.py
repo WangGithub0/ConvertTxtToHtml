ab8097f69ef0c7b850935d9b2171677f9d0b0b8e
iqrarasool04 <iqrarasool45@gmail.com>
2023-10-05 21:44:16 +0500

    change3

--- convertTxtToHtml/configFeature.py
+++ convertTxtToHtml/configFeature.py
@@ -22,3 +22,32 @@ def load_config_file(config_file_path):
         print(f"Error parsing TOML in '{config_file_path}'. Make sure it's valid TOML.")
         exit(1)
 
+def main():
+    # Parse command-line arguments
+    args = parse_command_line_args()
+
+    # If a config file is provided, load and use it
+    if args.config:
+        config_data = load_config_file(args.config)
+    else:
+        # Default values if config file not provided
+        config_data = {
+            "output": "./build",
+            "stylesheet": "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
+            "lang": "en",  # Default language
+        }
+
+    # Access config values
+    output_dir = config_data.get("output")
+    stylesheet_url = config_data.get("stylesheet")
+    lang = config_data.get("lang")
+
+    # Example: Using the config values in your application
+    print(f"Output Directory: {output_dir}")
+    print(f"Stylesheet URL: {stylesheet_url}")
+    print(f"Language: {lang}")
+
+    # Your code here: Use the config values in your application
+
+if __name__ == "__main__":
+    main()
