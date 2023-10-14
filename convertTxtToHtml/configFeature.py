90dcc846328193932f47e6caaee3b2233044c0d3
iqrarasool04 <iqrarasool45@gmail.com>
2023-10-14 10:33:53 +0500

    python to java conversion

--- convertTxtToHtml/configFeature.py
+++ convertTxtToHtml/configFeature.py
@@ -1,42 +1,48 @@
-import argparse
-import toml
+const arg = require("arg");
+const fs = require("fs").promises;
+const toml = require("toml");
 
-def parse_command_line_args():
-    parser = argparse.ArgumentParser(description="CLI Application with TOML Config File Support")
-    parser.add_argument("-c", "--config", help="Path to the TOML configuration file")
-    return parser.parse_args()
+function parseCommandArgs() {
+  const args = arg({
+    "--config": String,
+    "-c": "--config",
+  });
+  return args["--config"];
+}
 
-def load_config_file(config_file_path):
-    try:
-        with open(config_file_path, "r") as file:
-            config_data = toml.load(file)
-        return config_data
-    except FileNotFoundError:
-        print(f"Config file '{config_file_path}' not found.")
-        exit(1)
-    except toml.TomlDecodeError:
-        print(f"Error parsing TOML in '{config_file_path}'. Make sure it's valid TOML.")
-        exit(1)
-
-def main():
-    args = parse_command_line_args()
-    config_data = load_config_file(args.config) if args.config else {}
-
-    default_config = {
-        "output": "./build",
-        "stylesheet": "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
-        "lang": "en",
+async function loadConfigFile(configFilePath) {
+  try {
+    const configData = await fs.readFile(configFilePath, "utf8");
+    return toml.parse(configData);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      console.error("Config file ${configFilePath} not found.");
+    } else {
+      console.error("Error parsing TOML in ${configFilePath}. Make sure it's valid TOML.");
     }
+    process.exit(1);
+  }
+}
 
-    config = {**default_config, **config_data}
+function main() {
+  const configFilePath = parseCommandArgs();
+  const configData = configFilePath ? loadConfigFile(configFilePath) : {};
 
-    output_dir = config["output"]
-    stylesheet_url = config["stylesheet"]
-    lang = config["lang"]
+  const defaultConfig = {
+    output: "./build",
+    stylesheet: "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css",
+    lang: "en",
+  };
 
-    print(f"Output Directory: {output_dir}")
-    print(f"Stylesheet URL: {stylesheet_url}")
-    print(f"Language: {lang}")
+  const config = { ...defaultConfig, ...configData };
 
-if __name__ == "__main__":
-    main()
+  const outputDir = config.output;
+  const stylesheetURL = config.stylesheet;
+  const lang = config.lang;
+
+  console.log("Output Directory: ${outputDir}");
+  console.log("Stylesheet URL: ${stylesheetURL}");
+  console.log("Language: ${lang}");
+}
+
+main();
